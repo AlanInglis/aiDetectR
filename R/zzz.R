b7383ad00b34da76eff68bf85273d22364fba57d
Alan Inglis <alaninglis@MAC-9DD782.station>
2025-05-18 21:39:38 +0100

    more python setup

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,22 @@
 .onLoad <- function(libname, pkgname) {
   reticulate::use_condaenv("imgdet", required = FALSE)
+  
+  torch        <<- reticulate::import("torch", delay_load = TRUE)
+  transformers <<- reticulate::import("transformers", delay_load = TRUE)
+  PIL_Image    <<- reticulate::import("PIL.Image", delay_load = TRUE)
+  
+  hf_model  <<- "Ateeqq/ai-vs-human-image-detector"
+  processor <<- transformers$AutoImageProcessor$from_pretrained(hf_model)
+  model     <<- transformers$AutoModelForImageClassification$from_pretrained(hf_model)
+  
+  device <<- torch$device(
+    if (torch$cuda$is_available()) {
+      "cuda"
+    } else if (torch$backends$mps$is_available()) {
+      "mps"
+    } else {
+      "cpu"
+    }
+  )
+  model$to(device)$eval()
 }
